30e43c40bb52bdbaf2a18922d88b622ae9d431eb
SimenKSM <110755778+SimenKSM@users.noreply.github.com>
2023-01-11 13:53:58 +0100

    Update model.js

--- Modul2-Jan-Feb23/Uke1/Prosjekt/model.js
+++ Modul2-Jan-Feb23/Uke1/Prosjekt/model.js
@@ -1,4 +1,66 @@
 
-<script>
-    
-</script>
+const model = {
+    //Tilstand app
+    app: {
+        currentPage: 'ControlPanel',
+        currentUser: null,
+
+    },
+    // Input Felter til hver side
+    inputs: {
+        generatorPage: {
+            questionArray: [
+                {
+                    questionText: 'Spørsmål A',
+                },
+            ],
+            selfDefined: {
+                isChecked: false,
+                admincomment: 'Kommentar til egendefinert svar',
+
+            },
+
+            isOver: {
+                date: '11.01.2023',
+                time: '12:00',
+
+            },
+        },
+        //Korrekt måte og sette opp dette, trengs det array (Fordeler / Ulemper ved om brukere kan velge flere alternativ og hva er best practice om det er tillat)
+        surveyPage: {
+            chosenAnswerID: '',
+            userComment: 'Hva brukeren svarte',
+        },
+
+        // Hva er riktig og gjøre her tenker lærer? Skal dette ligge i data / controller funksjon, burde det gjøres om en til knapp fra en select?
+        resultPage: {
+            stopSurvey: false
+        },
+    },
+    //Data
+    surveyData: [
+        {
+            id: 1,
+            title: 'placeholder tittel',
+            description: 'Mulighet hvis det trengs ekstra info i tillegg til tittel',
+            timeStamp: 'Dato / Tid ',
+            surveyQuestions: [
+                {
+                    questionID: 1,
+                    voteNo: null,
+                    questionText: 'Spørsmål A',
+                    preDefinedAnswers: [ //Bør dette ligge i et eget array med objekter, eller bare som et objekt i surveydata Arrayet?
+                        {
+                            questionText: 'Egendefinert svar'
+                        }
+                    ],
+
+                },
+                
+            ],
+        },
+    ],
+
+
+
+};
