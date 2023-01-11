8e5f0452ea86b4e826c4651e9eab80c262001b7c
SimenKSM <110755778+SimenKSM@users.noreply.github.com>
2023-01-11 14:52:28 +0100

    Update model.js

--- Modul2-Jan-Feb23/Uke1/Prosjekt/model.js
+++ Modul2-Jan-Feb23/Uke1/Prosjekt/model.js
@@ -8,36 +8,71 @@ const model = {
     },
     // Input Felter til hver side
     inputs: {
-        generatorPage: {
-            questionArray: [
-                {
-                    questionText: 'Spørsmål A',
-                },
-            ],
-            selfDefined: {
-                isChecked: false,
-                admincomment: 'Kommentar til egendefinert svar',
+        surveyTitle: "",
+        questionTitle: "",
+        choisesText: ["har du spist pizza?", "", ""], // hvis man trykker på legg til, blir det lagt til ett felt til i arrayet
+        selfDefined: false,
+        date: '',
+        time: '',
+        stopSurvey: false,
+    }
+    // generatorPage: { //Skal vi lage Generatorpage og Surveypage // eller bør alle inputs bare ligge her uten at de ligger i et objekt for en spesifikk side.
+    //     questionArray: [
+    //         {
+    //             questionText: 'Spørsmål A',
+    //         },
+    //     ],
+    //     question: '',
 
-            },
+    //     selfDefined: {
+    //         isChecked: false,
+    //         admincomment: 'Kommentar til egendefinert svar',
 
-            isOver: {
-                date: '11.01.2023',
-                time: '12:00',
+    //     },
 
-            },
-        },
-        //Korrekt måte og sette opp dette, trengs det array (Fordeler / Ulemper ved om brukere kan velge flere alternativ og hva er best practice om det er tillat)
-        surveyPage: {
-            chosenAnswerID: '',
-            userComment: 'Hva brukeren svarte',
-        },
+    //     isOver: {
 
-        // Hva er riktig og gjøre her tenker lærer? Skal dette ligge i data / controller funksjon, burde det gjøres om en til knapp fra en select?
-        resultPage: {
-            stopSurvey: false
-        },
-    },
+
+    //     },
+    // },
+    // //Korrekt måte og sette opp dette, trengs det array (Fordeler / Ulemper ved om brukere kan velge flere alternativ og hva er best practice om det er tillat)
+    // surveyPage: {
+    //     chosenAnswerID: '',
+    //     userComment: 'Hva brukeren svarte',
+    // },
+
+    // // Hva er riktig og gjøre her tenker lærer? Skal dette ligge i data / controller funksjon, burde det gjøres om en til knapp fra en select?
+    // resultPage: {
+
+    // },
+    ,
     //Data
+    surveyData: [
+        {
+            surveyId: 1,
+            isSelfDefined: false,
+            isActive: true,
+            title: 'Placeholder Tittel på meningsmålingen',
+            endDate: 'Placeholder Sluttdato om det er valgt',
+            surveyQuestions: [
+                {
+                    questionID: 1,
+                    voteNo: null,
+                    questionText: 'Placeholder SpørsmålText',
+                    preDefinedAnswers: [
+                        {
+                            answerText: 'Placeholder Egendefinert Svar'
+                        }
+                    ],
+
+                },
+
+
+            ],
+        },
+    ],
+    // alt SurveyData
+    /* 
     surveyData: [
         {
             id: 1,
@@ -48,19 +83,40 @@ const model = {
                 {
                     questionID: 1,
                     voteNo: null,
+                    totalVotes: null, // Bør dette skrives inn eller regner man det ut med en funksjon
                     questionText: 'Spørsmål A',
-                    preDefinedAnswers: [ //Bør dette ligge i et eget array med objekter, eller bare som et objekt i surveydata Arrayet?
-                        {
-                            questionText: 'Egendefinert svar'
-                        }
-                    ],
 
                 },
-                
+                {
+                    questionID: 2,
+                    voteNo: null,
+                    totalVotes: null, // Bør dette skrives inn eller regner man det ut med en funksjon
+                    questionText: 'Spørsmål B',
+
+                },
+                {
+                    questionID: 3,
+                    voteNo: null,
+                    totalVotes: null, // Bør dette skrives inn eller regner man det ut med en funksjon
+                    questionText: 'Spørsmål C',
+
+                },
+                {
+                    preDefinedAnswers: [ //Bør dette ligge i et eget array med objekter, eller bare som et objekt i surveydata Arrayet?
+
+                        {
+                            preDefinedAnswerID: 1,
+                            questionText: 'Egendefinert svar',
+                        },
+                        {
+                            preDefinedAnswerID: 2,
+                            questionText: 'Egendefinert svar',
+                        },
+                    ]
+                },
+
             ],
         },
     ],
-
-
-
+ */
 };
