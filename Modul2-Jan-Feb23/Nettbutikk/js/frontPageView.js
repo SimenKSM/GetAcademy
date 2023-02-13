bf86b8082cd365c7391da1669d5192bf3a1fe17f
SimenKSM <110755778+SimenKSM@users.noreply.github.com>
2023-02-13 14:47:53 +0100

    Update frontPageView.js

--- Modul2-Jan-Feb23/Nettbutikk/js/frontPageView.js
+++ Modul2-Jan-Feb23/Nettbutikk/js/frontPageView.js
@@ -6,7 +6,7 @@ function frontPageView() {
     
         <div class="maintext">
             <h3 class="header-content-center">dummy text (trenger?)</h3>
-            <img class="frontpage-image" src="https://g.acdn.no/obscura/API/dynamic/r1/ece5/tr_1000_2000_s_f/0000/haar/2022/10/22/5/1000946396.JPG?chk=64ABC1">
+            <img class="frontpage-image" src="https://images.finncdn.no/dynamic/1280w/2022/6/vertical-0/24/4/186/187/554_1748646342.jpg">
         </div>
     </div>
     <div class="frontpage-flex-center-wrapper">
