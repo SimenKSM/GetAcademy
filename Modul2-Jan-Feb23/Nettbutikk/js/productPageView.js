56ecfe6aaf0857e7e080c961c35cb5bff2e11c77
SimenKSM <110755778+SimenKSM@users.noreply.github.com>
2023-02-14 15:05:53 +0100

    Update productPageView.js

--- Modul2-Jan-Feb23/Nettbutikk/js/productPageView.js
+++ Modul2-Jan-Feb23/Nettbutikk/js/productPageView.js
@@ -10,7 +10,7 @@ function productPageView() {
             </div>
         </div>
         <div class="productpage-item-container">
-            <h3>Store sokker</h3>
+            <h3>Små sokker</h3>
             <div class="productpage-image-container">
             <img src="http://2.bp.blogspot.com/-cGRGo6iHLFg/Te8iYutHa9I/AAAAAAAAALE/4ztMi2uNOjg/s1600/016.JPG">
             </div>
